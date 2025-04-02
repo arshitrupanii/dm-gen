30c88a07b6b89f70d91e58af6a65d76a261e8d09
arshitrupanii <arshitrupani6@gmail.com>
2025-04-02 19:35:38 +0530

    update analy

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import { UserProvider } from './components/UserContext';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
           <Footer />
         </UserProvider>
       </body>
+      <Analytics/>
     </html>
   );
 }
